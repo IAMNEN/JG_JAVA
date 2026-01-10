5f99b138b43b19cf5ddb2bbcca2c13905f3221cd
Nen Pachani <nenpanchani@gmail.com>
2026-01-10 13:35:20 +0530

    user input

--- userinput.java
+++ userinput.java
@@ -1,22 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
 
-class userinput{
+public class userinput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter UserName ->");
-        System.out.println("Enter Password ->");
-        String username = "nen";
-        String password = "nen123";
-        String newusername = sc.nextLine();
-        String newpassword = sc.nextLine();
-
-        if (newpassword.equals(password) && newusername.equals(username)) {
-            System.out.println("Login Successful");
-            System.out.println("Welcome "+username);
-        }
-        else {
-            System.out.println("Login Failed");
-        }
+        System.out.print("Enter your Age: ");
+        int age = sc.nextInt();
+        System.out.print("Enter your RollNumber: ");
+        int rollnumber = sc.nextInt();
+        System.out.println("..........Student Details..........");
+        System.out.println("Age -> " + age);
+        System.out.println("RollNumber -> " + rollnumber);
 
     }
 }
