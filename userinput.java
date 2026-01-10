4d18b8e37525da6aa820a1267f22c8944d521328
Nen Pachani <nenpanchani@gmail.com>
2026-01-10 13:46:10 +0530

    user input

--- userinput.java
+++ userinput.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class userinput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your Age: ");
-        int age = sc.nextInt();
+
         System.out.print("Enter your RollNumber: ");
         int rollnumber = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("Enter your Name: ");
+        String name = sc.nextLine();
+
         System.out.println("..........Student Details..........");
-        System.out.println("Age -> " + age);
+        System.out.println("Name -> " + name);
         System.out.println("RollNumber -> " + rollnumber);
 
     }
