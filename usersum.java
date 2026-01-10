33d88be0e219756cd4b99e6281fb58dcbae49833
Nen Pachani <nenpanchani@gmail.com>
2026-01-10 13:27:28 +0530

    Sum

---
+++ usersum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class usersum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number : ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter Second number : ");
+        int n2 = sc.nextInt();
+        int ans = n1 + n2;
+        System.out.println("Ans -> "+ans);
+    }
+}
