18e74c4034a2b69f410d1666d83f3d9757d30a22
Nen Pachani <nenpanchani@gmail.com>
2026-01-30 15:44:19 +0530

    constructorex

---
+++ overloading.java
@@ -0,0 +1,21 @@
+public class overloading {
+    double a;
+    double b;
+    overloading(){
+        a = 1.0;
+        b = 1.0;
+    }
+    overloading(double length,double width){
+        a = length;
+        b = width;
+    }
+    void display(){
+        System.out.println("Length ->"+a);
+        System.out.println("Width ->"+b);
+        System.out.println("Ans ->"+a*b);
+    }
+    public static void main(String[] args) {
+        overloading s1 = new overloading();
+        s1.display();
+    }
+}
---
+++ staticblock.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class staticblock {
+    static{
+        System.out.println("Static block");
+    }
+    {
+        System.out.println("instant block");
+    }
+    staticblock(int y){
+        System.out.println("Within Parameterized Constructor");
+    }
+    staticblock(){
+        System.out.println("Within Default Constructor");
+    }
+    public static void main(String[] args) {
+        System.out.println("in main");
+        staticblock obj = new staticblock();
+        System.out.println("---------------------------");
+        staticblock obj2 = new staticblock();
+    }
+}
