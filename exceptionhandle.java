5dfe264c6693b02a32582fba14c382751e048ee6
Nen Pachani <nenpanchani@gmail.com>
2026-02-25 14:46:52 +0530

    Exception handle

---
+++ exceptionhandle.java
@@ -0,0 +1,13 @@
+public class exceptionhandle {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Hello");
+            int a = 15/0;
+            System.out.println("Welcome");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Fahhhhhhhhhhhhhhhhh");
+        }
+        System.out.println("Bye");
+    }
+}
