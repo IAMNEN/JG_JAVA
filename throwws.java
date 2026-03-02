7b5b5392004bafe41e125137ea8dfce0a154e802
Nen Pachani <nenpanchani@gmail.com>
2026-03-02 19:36:34 +0530

    throwwww

--- throwws.java
+++ throwws.java
@@ -1,8 +1,8 @@
-
 public class throwws {
     static void display(int a) throws IllegalArgumentException {
         if(a < 18){
-            throw new IllegalArgumentException("Age must be greater than 18");
+
+           throw new IllegalArgumentException("Age must be greater than 18");
         }else{
             System.out.println("Age " + a);
         }
