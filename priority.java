96da1a1be85989ea9983764ab72a748649ce4ca1
Nen Pachani <nenpanchani@gmail.com>
2026-03-18 22:59:13 +0530

    Just

--- priority.java
+++ priority.java
@@ -1,11 +1,14 @@
 class PriorityThread extends Thread {
     String name;
+    //Constructor
     public  PriorityThread (String name2) {
-     name = name2;
+        name = name2;
     }
+    //Run Method
     public void run(){
         for (int i = 0 ; i <= 3 ; i++){
-            System.out.println(name + "Priority :" + Thread.currentThread().getPriority() + "Count -" +i);
+            // In Side the Print Statement Use GETPriority Method
+            System.out.println(name + " Priority :" + Thread.currentThread().getPriority() + " Count - " +i+ "\n");
         }
     }
 }
@@ -16,6 +19,7 @@ public class priority {
         PriorityThread p2 = new PriorityThread("Normal Priority");
         PriorityThread p3 = new PriorityThread("High Priority");
 
+        //Use SERPriority Method
         p1.setPriority(Thread.MIN_PRIORITY);
         p2.setPriority(Thread.NORM_PRIORITY);
         p3.setPriority(Thread.MAX_PRIORITY);
--- throwws.java
+++ throwws.java
@@ -1,7 +1,6 @@
 public class throwws {
     static void display(int a) throws IllegalArgumentException {
         if(a < 18){
-
            throw new IllegalArgumentException("Age must be greater than 18");
         }else{
             System.out.println("Age " + a);
